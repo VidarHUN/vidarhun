69be2aa4e0ee5d8ac00a2439a412ff3bac944310
Richard Varadi <ricsi19981007@gmail.com>
2022-12-18 13:15:14 +0100

    Add about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,36 @@ import React from 'react'
 import {aboutData} from '../data';
 
 const About = () => {
+    // About data content
+    const {image, title, subtitle} = aboutData;
     return (
-        <section>
-            About
+        <section className='my-[30px] xl:mt-[100px]'
+        data-aos='fade-up'
+        data-aos-offset='350'
+        >
+            <div className='container mx-auto'>
+                <div className='bg-accent-secondary/[15%] rounded-[50px] min-h-[560px] px-12 pb-12 flex flex-col text-center xl:flex-row xl:items-center xl:text-left xl:gap-x-[60px] xl:pb-0'>
+                    {/* Image section */}
+                    <div className='flex-1' data-aos='zoom-in-left'>
+                        <img src={image} alt=''></img>
+                    </div>
+                    {/* Text */}
+                    <div className='flex-1 xl:pr-12'
+                    data-aos='fade-up'
+                    data-aos-delay='300'
+                    >
+                        <h2 className='h2 mb-10'>
+                            {title}
+                        </h2>
+                        <p className='max-w-[474px] mx-auto xl:mx-0'
+                        data-aos='fade-up'
+                        data-aos-delay='400'
+                        >
+                            {subtitle}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
